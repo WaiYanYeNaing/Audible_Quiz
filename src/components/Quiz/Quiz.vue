108d10c69029672241ea32f3483106b5b3a2f2b7
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2022-07-01 11:47:33 +0630

    update

--- src/components/Quiz/Quiz.vue
+++ src/components/Quiz/Quiz.vue
@@ -1,6 +1,14 @@
 <template>
   <div
-    class="h-screen flex flex-col justify-between items-center space-y-9 mt-[23vh] pb-10"
+    class="
+      h-screen
+      flex flex-col
+      justify-between
+      items-center
+      space-y-9
+      mt-[23vh]
+      pb-10
+    "
   >
     <div>
       <div class="text-[#fff] text-lg leading-snug text-center">
@@ -29,13 +37,47 @@
     <!-- Next Question -->
     <div @click="increment" class="relative flex items-center justify-center">
       <div
-        class="absolute bg-gradient-to-r from-darkgray to-gray animate-pulse w-28 h-28 rounded-full"
+        class="
+          absolute
+          bg-gradient-to-r
+          from-darkgray
+          to-gray
+          animate-pulse
+          w-28
+          h-28
+          rounded-full
+        "
       ></div>
       <div
-        class="absolute bg-gradient-to-r from-darkgray to-darkgray w-16 h-16 flex items-center justify-center rounded-full hover:scale-105 ease-in duration-100 transition cursor-pointer"
+        class="
+          absolute
+          bg-gradient-to-r
+          from-darkgray
+          to-darkgray
+          w-16
+          h-16
+          flex
+          items-center
+          justify-center
+          rounded-full
+          hover:scale-105
+          ease-in
+          duration-100
+          transition
+          cursor-pointer
+        "
       >
         <img
-          class="w-9 h-9 bg-yellow shadow-lg rounded-full pt-[8px] pb-[8px] px-2"
+          class="
+            w-9
+            h-9
+            bg-yellow
+            shadow-lg
+            rounded-full
+            pt-[8px]
+            pb-[8px]
+            px-2
+          "
           src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-1.png"
         />
       </div>
@@ -43,46 +85,48 @@
   </div>
 </template>
 <script setup>
-import Button from '../Button/Button.vue'
-import { ref } from 'vue'
+import Button from "../Button/Button.vue";
+import { ref } from "vue";
 
 // reactive state
-let index = ref(0)
-let selected_id = ref(null)
+let index = ref(0);
+let selected_id = ref(null);
 
 // functions that mutate state and trigger updates
 const increment = () => {
-  index.value++
-  selected_id.value = null
-}
-const Selected_Handler = (id, index) => {
-  if (items.value[index].Selected_Answer == '') {
-    selected_id.value = id
-    items.value[index].Selected_Answer = id
+  if (selected_id.value != null) {
+    index.value++;
+    selected_id.value = null;
   }
-}
+};
+const Selected_Handler = (id, index) => {
+  if (items.value[index].Selected_Answer == "") {
+    selected_id.value = id;
+    items.value[index].Selected_Answer = id;
+  }
+};
 
 let items = ref([
   {
     id: 0,
-    Title: 'And go!',
+    Title: "And go!",
     Question: "Who's Listening?",
     Answer: [
       {
         id: 1,
-        name: 'Euphoria',
+        name: "Euphoria",
       },
       {
         id: 2,
-        name: 'Angel Baby',
+        name: "Angel Baby",
       },
       {
         id: 3,
-        name: 'Beside You',
+        name: "Beside You",
       },
       {
         id: 4,
-        name: 'Ipad',
+        name: "Ipad",
       },
     ],
     Correct_Answer: 2,
@@ -90,24 +134,24 @@ let items = ref([
   },
   {
     id: 1,
-    Title: 'Just a couple more questions!',
+    Title: "Just a couple more questions!",
     Question: "Who's Listening?",
     Answer: [
       {
         id: 1,
-        name: 'Funny',
+        name: "Funny",
       },
       {
         id: 2,
-        name: 'Less of you',
+        name: "Less of you",
       },
       {
         id: 3,
-        name: '2 soon',
+        name: "2 soon",
       },
       {
         id: 4,
-        name: 'My You',
+        name: "My You",
       },
     ],
     Correct_Answer: 4,
@@ -115,24 +159,24 @@ let items = ref([
   },
   {
     id: 2,
-    Title: 'General Knowledge Quiz',
-    Question: 'What was Meta Platforms Inc formerly known as?',
+    Title: "General Knowledge Quiz",
+    Question: "What was Meta Platforms Inc formerly known as?",
     Answer: [
       {
         id: 1,
-        name: 'Facebook',
+        name: "Facebook",
       },
       {
         id: 2,
-        name: 'Instagram',
+        name: "Instagram",
       },
       {
         id: 3,
-        name: 'Line',
+        name: "Line",
       },
       {
         id: 4,
-        name: 'Telegram',
+        name: "Telegram",
       },
     ],
     Correct_Answer: 1,
@@ -140,24 +184,24 @@ let items = ref([
   },
   {
     id: 3,
-    Title: 'General Knowledge Quiz',
-    Question: 'What does CIA stand for?',
+    Title: "General Knowledge Quiz",
+    Question: "What does CIA stand for?",
     Answer: [
       {
         id: 1,
-        name: 'Center Intelligence Agency',
+        name: "Center Intelligence Agency",
       },
       {
         id: 2,
-        name: 'Civil Intelligence Agancy',
+        name: "Civil Intelligence Agancy",
       },
       {
         id: 3,
-        name: 'Central Intelligence Agency',
+        name: "Central Intelligence Agency",
       },
       {
         id: 4,
-        name: 'Cute Information Agency',
+        name: "Cute Information Agency",
       },
     ],
     Correct_Answer: 3,
@@ -165,24 +209,24 @@ let items = ref([
   },
   {
     id: 4,
-    Title: 'General Knowledge Quiz',
-    Question: 'What is a young giraffe called?',
+    Title: "General Knowledge Quiz",
+    Question: "What is a young giraffe called?",
     Answer: [
       {
         id: 1,
-        name: 'Baby giraffe',
+        name: "Baby giraffe",
       },
       {
         id: 2,
-        name: 'A calf',
+        name: "A calf",
       },
       {
         id: 3,
-        name: 'Small giraffe',
+        name: "Small giraffe",
       },
       {
         id: 4,
-        name: 'Baby Long neck',
+        name: "Baby Long neck",
       },
     ],
     Correct_Answer: 2,
@@ -190,24 +234,24 @@ let items = ref([
   },
   {
     id: 5,
-    Title: 'General Knowledge Quiz',
-    Question: 'Pyrophobia is the fear of what?',
+    Title: "General Knowledge Quiz",
+    Question: "Pyrophobia is the fear of what?",
     Answer: [
       {
         id: 1,
-        name: 'Height',
+        name: "Height",
       },
       {
         id: 2,
-        name: 'Anime',
+        name: "Anime",
       },
       {
         id: 3,
-        name: 'Water',
+        name: "Water",
       },
       {
         id: 4,
-        name: 'Fire',
+        name: "Fire",
       },
     ],
     Correct_Answer: 4,
@@ -215,24 +259,24 @@ let items = ref([
   },
   {
     id: 6,
-    Title: 'General Knowledge Quiz',
-    Question: 'According to George Orwell, who is watching us?',
+    Title: "General Knowledge Quiz",
+    Question: "According to George Orwell, who is watching us?",
     Answer: [
       {
         id: 1,
-        name: 'Big Brother',
+        name: "Big Brother",
       },
       {
         id: 2,
-        name: 'Big Sister',
+        name: "Big Sister",
       },
       {
         id: 3,
-        name: 'Small Brother',
+        name: "Small Brother",
       },
       {
         id: 4,
-        name: 'Big Daddy',
+        name: "Big Daddy",
       },
     ],
     Correct_Answer: 1,
@@ -240,25 +284,25 @@ let items = ref([
   },
   {
     id: 7,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question:
-      'Randall Flag, Jack Torrance and John Coffey are all characters created by which author?',
+      "Randall Flag, Jack Torrance and John Coffey are all characters created by which author?",
     Answer: [
       {
         id: 1,
-        name: 'Stephen Hawking',
+        name: "Stephen Hawking",
       },
       {
         id: 2,
-        name: 'Stephen King',
+        name: "Stephen King",
       },
       {
         id: 3,
-        name: 'Stephen Chow',
+        name: "Stephen Chow",
       },
       {
         id: 4,
-        name: 'Stephen Curry',
+        name: "Stephen Curry",
       },
     ],
     Correct_Answer: 2,
@@ -266,20 +310,20 @@ let items = ref([
   },
   {
     id: 8,
-    Title: 'General Knowledge Quiz',
-    Question: 'What band was Harry Styles in before his solo career?',
+    Title: "General Knowledge Quiz",
+    Question: "What band was Harry Styles in before his solo career?",
     Answer: [
       {
         id: 1,
-        name: 'One Direction',
+        name: "One Direction",
       },
       {
         id: 2,
-        name: 'Two Direction',
+        name: "Two Direction",
       },
       {
         id: 3,
-        name: 'Linkin Park',
+        name: "Linkin Park",
       },
       {
         id: 4,
@@ -291,24 +335,24 @@ let items = ref([
   },
   {
     id: 9,
-    Title: 'General Knowledge Quiz',
-    Question: 'Who is the president of the United States?',
+    Title: "General Knowledge Quiz",
+    Question: "Who is the president of the United States?",
     Answer: [
       {
         id: 1,
-        name: 'Donald Trump',
+        name: "Donald Trump",
       },
       {
         id: 2,
-        name: 'Donald Duck',
+        name: "Donald Duck",
       },
       {
         id: 3,
-        name: 'Joe Biden',
+        name: "Joe Biden",
       },
       {
         id: 4,
-        name: 'Joe Alwyn',
+        name: "Joe Alwyn",
       },
     ],
     Correct_Answer: 3,
@@ -316,25 +360,25 @@ let items = ref([
   },
   {
     id: 10,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question:
-      'Which British actor will play Batman in the upcoming reboot directed by Matt Reeves?',
+      "Which British actor will play Batman in the upcoming reboot directed by Matt Reeves?",
     Answer: [
       {
         id: 1,
-        name: 'Donald Trump',
+        name: "Donald Trump",
       },
       {
         id: 2,
-        name: 'Ben Affleck',
+        name: "Ben Affleck",
       },
       {
         id: 3,
-        name: 'Christian Bale',
+        name: "Christian Bale",
       },
       {
         id: 4,
-        name: 'Robert Pattinson',
+        name: "Robert Pattinson",
       },
     ],
     Correct_Answer: 4,
@@ -342,25 +386,25 @@ let items = ref([
   },
   {
     id: 11,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question:
-      'How many countries are there in the region of Europe? (Recognised by the United Nations)',
+      "How many countries are there in the region of Europe? (Recognised by the United Nations)",
     Answer: [
       {
         id: 1,
-        name: '90',
+        name: "90",
       },
       {
         id: 2,
-        name: '40',
+        name: "40",
       },
       {
         id: 3,
-        name: '30',
+        name: "30",
       },
       {
         id: 4,
-        name: '45',
+        name: "45",
       },
     ],
     Correct_Answer: 2,
@@ -368,25 +412,25 @@ let items = ref([
   },
   {
     id: 12,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question:
-      'How many permanent members are there on the UN security council?',
+      "How many permanent members are there on the UN security council?",
     Answer: [
       {
         id: 1,
-        name: 'Six',
+        name: "Six",
       },
       {
         id: 2,
-        name: 'Four',
+        name: "Four",
       },
       {
         id: 3,
-        name: 'Five',
+        name: "Five",
       },
       {
         id: 4,
-        name: 'Eight',
+        name: "Eight",
       },
     ],
     Correct_Answer: 3,
@@ -394,24 +438,24 @@ let items = ref([
   },
   {
     id: 13,
-    Title: 'General Knowledge Quiz',
-    Question: 'How many centimetres in a metre?',
+    Title: "General Knowledge Quiz",
+    Question: "How many centimetres in a metre?",
     Answer: [
       {
         id: 1,
-        name: '10',
+        name: "10",
       },
       {
         id: 2,
-        name: '1000',
+        name: "1000",
       },
       {
         id: 3,
-        name: '-100',
+        name: "-100",
       },
       {
         id: 4,
-        name: '100',
+        name: "100",
       },
     ],
     Correct_Answer: 4,
@@ -419,24 +463,24 @@ let items = ref([
   },
   {
     id: 14,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question: "Who are Harry Potter's two best friends?",
     Answer: [
       {
         id: 1,
-        name: 'Doctor Strange and Wanda',
+        name: "Doctor Strange and Wanda",
       },
       {
         id: 2,
-        name: 'Tom and Jerry',
+        name: "Tom and Jerry",
       },
       {
         id: 3,
-        name: 'Sasuke and Sakura',
+        name: "Sasuke and Sakura",
       },
       {
         id: 4,
-        name: 'Ron and Hermione',
+        name: "Ron and Hermione",
       },
     ],
     Correct_Answer: 4,
@@ -444,24 +488,24 @@ let items = ref([
   },
   {
     id: 15,
-    Title: 'General Knowledge Quiz',
-    Question: 'What’s the name of the cowboy in Toy Story?',
+    Title: "General Knowledge Quiz",
+    Question: "What’s the name of the cowboy in Toy Story?",
     Answer: [
       {
         id: 1,
-        name: 'Woody',
+        name: "Woody",
       },
       {
         id: 2,
-        name: 'Booty',
+        name: "Booty",
       },
       {
         id: 3,
-        name: 'Buzz',
+        name: "Buzz",
       },
       {
         id: 4,
-        name: 'Shark',
+        name: "Shark",
       },
     ],
     Correct_Answer: 1,
@@ -469,24 +513,24 @@ let items = ref([
   },
   {
     id: 16,
-    Title: 'General Knowledge Quiz',
-    Question: 'How many planets are in our solar system?',
+    Title: "General Knowledge Quiz",
+    Question: "How many planets are in our solar system?",
     Answer: [
       {
         id: 1,
-        name: '6',
+        name: "6",
       },
       {
         id: 2,
-        name: '7',
+        name: "7",
       },
       {
         id: 3,
-        name: '8',
+        name: "8",
       },
       {
         id: 4,
-        name: '9',
+        name: "9",
       },
     ],
     Correct_Answer: 3,
@@ -494,24 +538,24 @@ let items = ref([
   },
   {
     id: 17,
-    Title: 'General Knowledge Quiz',
-    Question: 'How many elements are in the periodic table?',
+    Title: "General Knowledge Quiz",
+    Question: "How many elements are in the periodic table?",
     Answer: [
       {
         id: 1,
-        name: '118',
+        name: "118",
       },
       {
         id: 2,
-        name: '181',
+        name: "181",
       },
       {
         id: 3,
-        name: '119',
+        name: "119",
       },
       {
         id: 4,
-        name: '199',
+        name: "199",
       },
     ],
     Correct_Answer: 1,
@@ -519,24 +563,24 @@ let items = ref([
   },
   {
     id: 18,
-    Title: 'General Knowledge Quiz',
-    Question: 'How many hearts does an octopus have?',
+    Title: "General Knowledge Quiz",
+    Question: "How many hearts does an octopus have?",
     Answer: [
       {
         id: 1,
-        name: '0',
+        name: "0",
       },
       {
         id: 2,
-        name: '1',
+        name: "1",
       },
       {
         id: 3,
-        name: '2',
+        name: "2",
       },
       {
         id: 4,
-        name: '3',
+        name: "3",
       },
     ],
     Correct_Answer: 4,
@@ -544,25 +588,25 @@ let items = ref([
   },
   {
     id: 19,
-    Title: 'General Knowledge Quiz',
+    Title: "General Knowledge Quiz",
     Question:
-      'Name the actress who plays the role of Mike Wheeler in Stranger Things.',
+      "Name the actress who plays the role of Mike Wheeler in Stranger Things.",
     Answer: [
       {
         id: 1,
-        name: 'Fat Wolfhard',
+        name: "Fat Wolfhard",
       },
       {
         id: 2,
-        name: 'Finn Doghard',
+        name: "Finn Doghard",
       },
       {
         id: 3,
-        name: 'Finn Wolfeasy',
+        name: "Finn Wolfeasy",
       },
       {
         id: 4,
-        name: 'Finn Wolfhard',
+        name: "Finn Wolfhard",
       },
     ],
     Correct_Answer: 4,
@@ -570,24 +614,24 @@ let items = ref([
   },
   {
     id: 20,
-    Title: 'General Knowledge Quiz',
-    Question: 'Who killed Tony Stark’s parents?',
+    Title: "General Knowledge Quiz",
+    Question: "Who killed Tony Stark’s parents?",
     Answer: [
       {
         id: 1,
-        name: 'The Summer Soldier',
+        name: "The Summer Soldier",
       },
       {
         id: 2,
-        name: 'The Rainy Soldier',
+        name: "The Rainy Soldier",
       },
       {
         id: 3,
-        name: 'The Winter Soldier',
+        name: "The Winter Soldier",
       },
       {
         id: 4,
-        name: 'Captain America',
+        name: "Captain America",
       },
     ],
     Correct_Answer: 3,
@@ -595,24 +639,24 @@ let items = ref([
   },
   {
     id: 21,
-    Title: 'General Knowledge Quiz',
-    Question: 'What is the collective name for a group of crows?',
+    Title: "General Knowledge Quiz",
+    Question: "What is the collective name for a group of crows?",
     Answer: [
       {
         id: 1,
-        name: 'A killer',
+        name: "A killer",
       },
       {
         id: 2,
-        name: 'A Horde',
+        name: "A Horde",
       },
       {
         id: 3,
-        name: 'A murder',
+        name: "A murder",
       },
       {
         id: 4,
-        name: 'A corw group',
+        name: "A corw group",
       },
     ],
     Correct_Answer: 3,
@@ -620,28 +664,28 @@ let items = ref([
   },
   {
     id: 22,
-    Title: 'General Knowledge Quiz',
-    Question: 'How many sides does an octagon have?',
+    Title: "General Knowledge Quiz",
+    Question: "How many sides does an octagon have?",
     Answer: [
       {
         id: 1,
-        name: '8',
+        name: "8",
       },
       {
         id: 2,
-        name: '9',
+        name: "9",
       },
       {
         id: 3,
-        name: '10',
+        name: "10",
       },
       {
         id: 4,
-        name: '11',
+        name: "11",
       },
     ],
     Correct_Answer: 1,
     Selected_Answer: 0,
   },
-])
+]);
 </script>
